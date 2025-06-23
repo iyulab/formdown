ee3a0506dec7aca0ef050236c7e0d5071e8f8dd0
uj <caveman@iyulab.com>
2025-06-23 11:45:42 +0900

    cicd update

--- site/next.config.ts
+++ site/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   transpilePackages: ['@formdown/core', '@formdown/ui', '@formdown/editor'],
   output: 'export',
+  distDir: 'publish',
   trailingSlash: true,
   images: {
     unoptimized: true
